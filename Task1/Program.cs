bd29c628e959d38f8eefe1085a6c4a5b5daf6904
evgen <evgen0404@mail.ru>
2023-12-26 18:59:44 +0300

    Решили 1 задачу

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+
+
+using System;
+
+class Program
+
+{
+    static void Main()
+    {
+       Console.Write("Введите число а: ");
+
+       int a = Convert.ToInt32(Console.ReadLine());
+       if(a % 7 == 0 && a % 23 == 0)
+       {
+        Console.WriteLine("Да");
+       }
+       else
+       {
+         Console.WriteLine("Нет");
+       }
+    }
+} 
+        
+
+
+
+   
+    
+           
+     
+     
+       
+      
+   
